6f3f1d9c051bdcd91bcbf9bdab0f6321d7cd30b9
chenzhou <chenzhouwy@163.com>
2018-11-13 13:40:18 +0800

    spring data redis

--- hadoop_service_bean/src/main/java/com/jeninfo/hadoopservice/model/User.java
+++ hadoop_service_bean/src/main/java/com/jeninfo/hadoopservice/model/User.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 /**
  * @author chenzhou
  */
@@ -12,7 +14,7 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @Data
 @Accessors(chain = true)
-public class User {
+public class User implements Serializable {
     private Integer id;
 
     private String name;
--- hadoop_service_provider/src/test/java/com/jeninfo/hadoopservice/mybatis/MybatisTest.java
+++ hadoop_service_provider/src/test/java/com/jeninfo/hadoopservice/mybatis/MybatisTest.java
@@ -1,16 +1,20 @@
 package com.jeninfo.hadoopservice.mybatis;
 
+import com.google.gson.Gson;
 import com.jeninfo.hadoopservice.dao.UserMapper;
 import com.jeninfo.hadoopservice.model.Article;
 import com.jeninfo.hadoopservice.model.User;
 import com.jeninfo.hadoopservice.service.EsService;
 import com.jeninfo.hadoopservice.service.KafKaService;
+import com.jeninfo.hadoopservice.service.UserService;
 import io.searchbox.client.JestClient;
 import io.searchbox.core.Index;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -18,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chenzhou
@@ -26,12 +31,22 @@ import java.util.List;
 @SpringBootTest
 public class MybatisTest {
     @Autowired
-    private EsService esService;
+    private UserService userService;
+
     @Autowired
-    private KafKaService kafKaService;
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     public void test01() {
-        kafKaService.send("hahahah");
+        //stringRedisTemplate.opsForValue().set("aa", "案件啊哈哈");
+        //stringRedisTemplate.expire("aa",1000*10, TimeUnit.MILLISECONDS);
+        String index = stringRedisTemplate.opsForList().index("list01", 1);
+        System.out.println(index);
+
+        Object o = stringRedisTemplate.opsForHash().get("user", "id");
+        System.out.println(o);
     }
 }
