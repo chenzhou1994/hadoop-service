26b0d2b3c593d2e98a5593c4714c5f91b0950465
chenzhou <chenzhouwy@163.com>
2018-11-24 15:01:27 +0800

    将provider注册进eureka

--- hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/HadoopTestApplication.java
+++ hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/HadoopTestApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chenzhou
  */
 @MapperScan("com.jeninfo.hadoopservice.dao")
 @SpringBootApplication
+@EnableEurekaClient
 public class HadoopTestApplication {
 
     public static void main(String[] args) {
