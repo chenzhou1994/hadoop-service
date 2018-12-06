435e79388c5efd9f451210460b9f5efbcbb8ca20
chenzhou <chenzhouwy@163.com>
2018-12-06 11:38:22 +0800

    完善euraka服务注册发现

--- hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/HadoopTestApplication.java
+++ hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/HadoopTestApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @MapperScan("com.jeninfo.hadoopservice.dao")
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class HadoopTestApplication {
 
     public static void main(String[] args) {
--- hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/controller/UserController.java
+++ hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/controller/UserController.java
@@ -3,7 +3,11 @@ package com.jeninfo.hadoopservice.controller;
 import com.jeninfo.hadoopservice.Msg;
 import com.jeninfo.hadoopservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author chenzhou
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/select/all", method = RequestMethod.GET)
     public Msg selectAll(@RequestParam(value = "page", required = false, defaultValue = "1") String page, @RequestParam(value = "pageSize", required = false, defaultValue = "10") String pageSize) {
@@ -22,5 +28,19 @@ public class UserController {
     @RequestMapping(value = "/select/{id}", method = RequestMethod.GET)
     public Msg selectById(@PathVariable("id") String id) {
         return Msg.renderSuccess("处理成功", 0x342, userService.selectById(id));
+    }
+
+
+    @RequestMapping(value = "/select/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("HADOOP_SERVICE_PROVIDER");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
     }
 }
