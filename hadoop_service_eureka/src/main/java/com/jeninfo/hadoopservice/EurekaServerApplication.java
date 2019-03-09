49bce43317059203a7e07d640ba473b8dc88aee2
chenzhou <chenzhouwy@163.com>
2019-03-09 16:56:32 +0800

    modify eureka

--- hadoop_service_eureka/src/main/java/com/jeninfo/hadoopservice/EurekaServerApplication.java
+++ hadoop_service_eureka/src/main/java/com/jeninfo/hadoopservice/EurekaServerApplication.java
@@ -11,8 +11,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerTest {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerTest.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/HadoopServiceProviderApplication.java
+++ hadoop_service_provider/src/main/java/com/jeninfo/hadoopservice/HadoopServiceProviderApplication.java
@@ -3,12 +3,15 @@ package com.jeninfo.hadoopservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chenzhou
  */
 @MapperScan("com.jeninfo.hadoopservice.dao")
 @SpringBootApplication
+@EnableEurekaClient
 public class HadoopServiceProviderApplication {
 
     public static void main(String[] args) {
