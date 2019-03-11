a9f2cf461f7a53d4db4b0d1c429824de44878a70
chenzhou <chenzhouwy@163.com>
2019-03-11 17:50:32 +0800

    fix some bug

--- hadoop_service_hdfs_provider/src/main/java/com/jeninfo/hadoopservice/HdfsServiceApplication.java
+++ hadoop_service_hdfs_provider/src/main/java/com/jeninfo/hadoopservice/HdfsServiceApplication.java
@@ -3,7 +3,6 @@ package com.jeninfo.hadoopservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -19,5 +18,4 @@ public class HdfsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HdfsServiceApplication.class, args);
     }
-
 }
--- hadoop_service_hdfs_provider/src/main/java/com/jeninfo/hadoopservice/service/HdfsService.java
+++ hadoop_service_hdfs_provider/src/main/java/com/jeninfo/hadoopservice/service/HdfsService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
